135487fd1a5a27715d57b555f96bd07976b12670
Peter Pickerill <peterpickerill2016@gmail.com>
2018-04-12 08:44:45 +0200

    Added API HANDLER

---
+++ src/org/repominer/com/APIHandler.java
@@ -0,0 +1,51 @@
+package org.repominer.com;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class APIHandler {
+	private String apiKey = null;
+	private String user = null;
+	private String repo = null;
+	public APIHandler(String apiKey, String user, String repo) {
+		this.apiKey = apiKey;
+		this.user = user;
+		this.repo = repo;
+	}
+	public String getCommits() {
+		String url = "/repos/:owner/:repo/git/commits";
+		url = url.replace(":owner", this.user);
+		url = url.replace(":repo", this.repo);
+		
+		return sendPostRequest(url, null);
+		
+	}
+	public static String sendPostRequest(String requestUrl, String payload) {
+		StringBuffer jsonString = new StringBuffer();
+	    try {
+	        URL url = new URL(requestUrl);
+	        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+	        connection.setDoInput(true);
+	        connection.setDoOutput(true);
+	        connection.setRequestMethod("POST");
+	        connection.setRequestProperty("Accept", "application/json");
+	        connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+	        //OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
+	        //writer.write(payload);
+	        //writer.close();
+	        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	        String line;
+	        while ((line = br.readLine()) != null) {
+	                jsonString.append(line);
+	        }
+	        br.close();
+	        connection.disconnect();
+	    } catch (Exception e) {
+	            throw new RuntimeException(e.getMessage());
+	    }
+	    return jsonString.toString();
+	}
+}
--- src/org/repominer/com/Main.java
+++ src/org/repominer/com/Main.java
@@ -5,6 +5,7 @@ import java.io.File;
 public class Main {
 
 	public static void main(String[] args) {
+		String accessToken = "";
 		//Setup[] 
 		String userHome = System.getProperty("user.home");
 		new File(userHome.concat(File.separatorChar + "RepoMiner")).mkdir();
@@ -12,8 +13,10 @@ public class Main {
 		new File(userHome.concat(File.separatorChar +"RepoMiner" + File.separatorChar + "analysis")).mkdir();
 		System.out.println("Starting To Mine Github");
 		GitHubRepo githubrepo = new GitHubRepo("https://github.com/joshuaju/SEP_webclient");
-		githubrepo.cloneProject();
-		githubrepo.analyse();
+		APIHandler apiHandler = new APIHandler(accessToken, "joshuaju", "SEP_webclient");
+		apiHandler.getCommits();
+		//githubrepo.cloneProject();
+		//githubrepo.analyse();
 		
 	}
 }
