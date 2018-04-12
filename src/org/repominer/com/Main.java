83cea061302b979d74c05b9ee1af263e553c9a10
Peter Pickerill <peterpickerill2016@gmail.com>
2018-04-12 17:18:57 +0200

    Added Server

--- src/org/repominer/com/Main.java
+++ src/org/repominer/com/Main.java
@@ -1,11 +1,15 @@
 package org.repominer.com;
 
 import java.io.File;
+import java.io.IOException;
+
 import org.repominer.visualiser.*;
+
+import com.repominer.server.Server;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String accessToken = "c0506854272cca9f5c0680e2187e55c81ccfcfb2";
 		//Setup[] 
 		System.out.println(PieChartGenerate.generate());
@@ -20,6 +24,7 @@ public class Main {
 		//apiHandler.getCommits();
 		githubrepo.cloneProject();
 		githubrepo.analyse();
+		Server server = new Server();
 		
 	}
 }
